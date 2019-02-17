1756f44afc5ebbb267e7efe8f0ccaca79ece6d06
JustinSHong <jhong2009@gmail.com>
2019-02-17 18:29:25 -0500

    attempt to generate json files

--- src/index.js
+++ src/index.js
@@ -3,31 +3,29 @@ import ReactDOM from "react-dom";
 import { IntlProvider, addLocaleData } from "react-intl";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
-import en from "react-intl/local-data/en";
-import es from "react-intl/locale-data/es";
+// import en from "react-intl/local-data/en";
+// import es from "react-intl/locale-data/es";
 
-import localeData from "./../public/locales/data.json";
-
-addLocaleData([...en, ...es]);
+// addLocaleData([...en, ...es]);
 
 // Define user's language. Different browsers have the user locale defined
 // on different fields on the `navigator` object, so we make sure to account
 // for these different by checking all of them
-const language =
-    (navigator.languages && navigator.languages[0]) ||
-    navigator.language ||
-    navigator.userLanguage;
+// const language =
+//     (navigator.languages && navigator.languages[0]) ||
+//     navigator.language ||
+//     navigator.userLanguage;
 
 // Split locales with a region code
-const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+// const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
 
 // Try full locale, try locale without region code, fallback to 'en'
-const messages =
-    localeData[languageWithoutRegionCode] ||
-    localeData[language] ||
-    localeData.en;
+// const messages =
+//     localeData[languageWithoutRegionCode] ||
+//     localeData[language] ||
+//     localeData.en;
 
 ReactDOM.render(
     <IntlProvider>
@@ -41,4 +39,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
